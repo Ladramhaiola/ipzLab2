ccee7b9dd755d410ee6e2b7cf8f434b144b00dbf
Andriy <andriy380@gmail.com>
2018-04-01 19:38:58 +0300

    a bit of style

--- gulpfile.js
+++ gulpfile.js
@@ -17,51 +17,51 @@ ignore = require('gulp-ignore'),
 newer = require('gulp-newer')
 
 gulp.task('serve', ()=>{
-    gulp.src('public')
-    .pipe(webserver({
-        livereload: true,
-        directoryListing: false,
-        open: true,
-        host: 'localhost',
-        port: 8080,
-        fallback: 'index.min.html'
-    }))
+    return gulp.src('public')
+        .pipe(webserver({
+            livereload: true,
+            directoryListing: false,
+            open: true,
+            host: 'localhost',
+            port: 8080,
+            fallback: 'index.min.html'
+        }))
 })
 
 gulp.task('default', ['watch', 'serve'])
 
 gulp.task('html', ()=>{
     return gulp.src('./assets/template/*.html') // read all files matching given pattern
-    .pipe(debug({title: 'src'}))                // show what is going on
-    .pipe(ignore.exclude('*.min.html'))         // ignore already min files
-    .pipe(minifyHtml())
-    .pipe(debug({title: 'minify'}))
-    .pipe(concat('index.html'))          // compile all html files to single
-    .pipe(debug({title: 'concat'}))
-    .pipe(rename({suffix: '.min'}))      // fname +.min +.html
-    .pipe(gulp.dest('./public'))         // write result to destination folder
+        .pipe(debug({title: 'src'}))                // show what is going on
+        .pipe(ignore.exclude('*.min.html'))         // ignore already min files
+        .pipe(minifyHtml())
+        .pipe(debug({title: 'minify'}))
+        .pipe(concat('index.html'))          // compile all html files to single
+        .pipe(debug({title: 'concat'}))
+        .pipe(rename({suffix: '.min'}))      // fname +.min +.html
+        .pipe(gulp.dest('./public'))         // write result to destination folder
     // .pipe(notify({message:'HTML minification completed'}))
 })
 
 gulp.task('styles', ()=>{
     return gulp.src('./assets/stylus/*.styl')
-    .pipe(sourcemap.init())
-    .pipe(stylus())
-    .pipe(minifyCss())
-    .pipe(concat('style.css'))
-    .pipe(sourcemap.write())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./public'))
-    // .pipe(notify({message:'CSS minification completed'}))
+        .pipe(sourcemap.init())
+        .pipe(stylus())
+        .pipe(minifyCss())
+        .pipe(concat('style.css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(sourcemap.write('.'))
+        .pipe(gulp.dest('./public'))
+        // .pipe(notify({message:'CSS minification completed'}))
 })
 
 gulp.task('js', ()=>{
     return gulp.src('./assets/js/*.js')
-    .pipe(babel())
-    .pipe(concat('app.js'))
-    .pipe(rename({suffix:'.min'}))
-    .pipe(gulp.dest('./public'))
-    // .pipe(notify({message:'ES6 compilation and minification completed'}))
+        .pipe(babel())
+        .pipe(concat('app.js'))
+        .pipe(rename({suffix:'.min'}))
+        .pipe(gulp.dest('./public'))
+        // .pipe(notify({message:'ES6 compilation and minification completed'}))
 })
 
 gulp.task('watch', ()=>{ // recompile files on changes and livereload them
